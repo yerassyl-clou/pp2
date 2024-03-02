5612779b2d21190ad1de1fc58db7ad548968510a
Yerassyl <babayevyerasyl@gmail.com>
2024-03-02 14:53:51 +0600

    lab6

--- labs/lab6/funcs/1.3.1.py
+++ labs/lab6/funcs/1.3.1.py
@@ -1,9 +1,9 @@
-a = (1,2,3,4,5)
+a = [1,2,3,4,5]
 
 lst = (a[:1])
 
 for x in a[1:]:
     lst *= x
     
-#print(lst)
+print(lst)
 print(sum(lst))
--- labs/lab6/funcs/1.3.2.py
+++ labs/lab6/funcs/1.3.2.py
@@ -1,9 +1,10 @@
-a = (1,2,3,4,5)
+a = [1,2,3,4,5]
 
-lst = (a[:1])
+lst = [a[:1]]
 
 for x in a[1:]:
-    lst *= x
+
+    for y in range(x):
+        lst.append(x)
     
-#print(lst)
-print(sum(lst))
+print(lst)
