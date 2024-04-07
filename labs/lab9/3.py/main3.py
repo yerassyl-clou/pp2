f9db36ee97433d7894d42e14f3122405f26b6ebc
Yerassyl <babayevyerasyl@gmail.com>
2024-04-07 23:29:03 +0500

    upd

--- labs/lab9/3.py/main3.py
+++ labs/lab9/3.py/main3.py
@@ -16,6 +16,11 @@ def drawSquare(screen, color, x1, y1, x2, y2):
     square = [(x1, y1), (x2, y1), (x2, y2), (x1, y2)]
     pygame.draw.polygon(screen, color, square, 5)
 
+def getTriangle(x1, y1, x2, y2):
+    x3 = x1
+    y3 = y2
+    return [(x1, y1), (x2, y2), (x3, y3)]
+
 pygame.init()
 screen = pygame.display.set_mode((1000, 750))
 another_layer = pygame.Surface((1000, 750))
@@ -47,8 +52,6 @@ while not done:
 
         pressed = pygame.key.get_pressed()
 
-
-
         color = colors[colorCnt]
 
         if pressed[pygame.K_c]:                                                     #change colors 
@@ -57,18 +60,19 @@ while not done:
             if colorCnt >= len(colors) - 1:
                 colorCnt = 0
 
-        if pressed[pygame.K_SPACE]:                                                 #swith btw rect and circle 
-            eventType = (eventType + 1) % 3
+        if pressed[pygame.K_SPACE]:                                                 #switch between rect and circle 
+            eventType = (eventType + 1) % 4
+            
 
         if pressed[pygame.K_e]:                                                     #switch to eraser 
-            eventType = 3
+            eventType = 4
 
         if pressed[pygame.K_r]:                                                     #clear screen
                 screen.fill("black")
                 another_layer.fill("black")
 
         if eventType == 0:                                                           #draw rectangle 
-            pygame.draw.rect(screen, "black", (10, 10, 20, 30))
+            pygame.draw.rect(screen, "black", (10, 10, 40, 40))
             pygame.draw.rect(screen, color, (10, 10, 20, 30), 2)
             pygame.draw.rect(another_layer, color, (10, 10, 20, 30), 2)
 
@@ -91,8 +95,8 @@ while not done:
                         pygame.draw.rect(screen, color, pygame.Rect(getRectangle(x1, y1, x2, y2)), 5)
 
 
-        if eventType == 1:                                                                      #circle
-            pygame.draw.rect(screen, "black", (10, 10, 20, 30))
+        if eventType == 1:                                                                      #draw circle
+            pygame.draw.rect(screen, "black", (10, 10, 40, 40))
             pygame.draw.circle(screen, color, (20, 20), 10, 2)
             pygame.draw.circle(another_layer, color, (20, 20), 10, 2)
 
@@ -114,8 +118,8 @@ while not done:
                         screen.blit(another_layer, (0, 0))    
                         pygame.draw.ellipse(screen, color, pygame.Rect(getRectangle(x1, y1, x2, y2)), 5)
                        
-        if eventType == 3:                                                                                        #eraser
-            pygame.draw.rect(screen, "black", (10, 10, 20, 30))
+        if eventType == 4:                                                                                        #eraser
+            pygame.draw.rect(screen, "black", (10, 10, 40, 40))
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1: 
@@ -139,7 +143,7 @@ while not done:
                     pygame.draw.line(screen, "black", (x1, y1), (x2, y2), 15)
 
         if eventType == 2:                                                                                        #square
-            pygame.draw.rect(screen, "black", (10, 10, 20, 30))
+            pygame.draw.rect(screen, "black", (10, 10, 40, 40))
             pygame.draw.rect(screen, color, (10, 10, 20, 20), 2)
             pygame.draw.rect(another_layer, color, (10, 10, 20, 20), 2)
 
@@ -161,6 +165,29 @@ while not done:
                     screen.blit(another_layer, (0, 0))
                     drawSquare(screen, color, x1, y1, x2, y2)
 
-            
+        if eventType == 3:                                                                                        #right triangle
+            pygame.draw.rect(screen, "black", (10, 10, 40, 40))
+            pygame.draw.polygon(screen, color, getTriangle(10, 10, 20, 30), 2)
+            pygame.draw.polygon(another_layer, color, getTriangle(10, 10, 20, 30), 2)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1: 
+                    x1 = event.pos[0]
+                    y1 = event.pos[1]
+                    isMouseDown = True
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    isMouseDown = False
+                    another_layer.blit(screen, (0, 0))
+
+            if event.type == pygame.MOUSEMOTION:
+                if isMouseDown:
+                    x2 = event.pos[0]
+                    y2 = event.pos[1]
+                    screen.blit(another_layer, (0, 0))
+                    triangle = getTriangle(x1, y1, x2, y2)
+                    pygame.draw.polygon(screen, color, triangle, 5)
+
     pygame.display.flip()
     clock.tick(120)
